fc6b13f1fe54b9e26e4bd3f022c1afbd6d86760e
cuiqijia <1097993489@qq.com>
2019-07-10 17:22:57 +0800

    heima

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,1 @@
 // 配置路由
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '@/views/login'
-
-Vue.use(VueRouter)
-const router = new VueRouter({
-  routes: [
-    { name: 'login', path: '/login', comment: Login }
-  ]
-})
-export default router
